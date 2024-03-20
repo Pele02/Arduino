61ec3dfd51b969f8e9660fdec512997fb9ba0dd6
Pele02 <pelealex02@gmail.com>
2024-03-20 21:25:28 +0200

    Added Temp Sensor
    Added the Temp Sensor to the physical setup and added the right libraries(DHT11) for it`s use.

--- SyncreticProject/SyncreticProject.ino
+++ SyncreticProject/SyncreticProject.ino
@@ -1,27 +1,40 @@
-#include <Wire.h>
-#include <Adafruit_LiquidCrystal.h>
 
-const int temperaturaPin = A0; 
+#include <DHT11.h> //Librarie pentru a putea fi folosit senzorul DHT11
+#include <Adafruit_LiquidCrystal.h> //Librarie pentru lcd
+
+DHT11 dht11(A0);
 Adafruit_LiquidCrystal lcd(0);
 const int ledPin = 13;
 
 void setup() {
-  pinMode(temperaturaPin, INPUT);  // Setam pinul analogic A0 ca intrare pentru temperatura
   pinMode(ledPin, OUTPUT); // Setam pinul LED la iesire
   Serial.begin(9600);  // Se initializeaza comunicarea seriala
   lcd.begin(16, 2);
 }
 
 void loop() {
-  int temperaturaRaw = analogRead(temperaturaPin);  // Citim valoarea analogica
-  
-  // Convertim valoarea citita, aproximata
-  float temperaturaCelsius = temperaturaRaw * 0.489 - 50; 
-  
+  //Variabile
+  int temperature = 0;
+  int humidity = 0;
+
+  //Citire date senzor
+  int result = dht11.readTemperatureHumidity(temperature, humidity);
+
+  if (result == 0) {
+    Serial.print("Temperatura: ");
+    Serial.print(temperature);
+    Serial.print(" °C\t Umiditate: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    } else {
+    // Print error message based on the error code.
+    Serial.println(DHT11::getErrorString(result));
+    }
+
   // Afisam temperatura pe ecranul LCD
   lcd.setCursor(0, 0); 
   lcd.print("Temp: "); 
-  lcd.print(temperaturaCelsius);
+  lcd.print(temperature);
   lcd.print(" C");
 
   // Verifica daca a fost primita o comanda
